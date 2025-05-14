d2947b2bfdac9fcafb7a1cfcd9dcd41812d322ba
Karanei-Kimutai <168116985+Karanei-Kimutai@users.noreply.github.com>
2025-05-14 21:56:23 +0300

    Added more functionality

--- LinkedLists/SinglyLinkedLists.py
+++ LinkedLists/SinglyLinkedLists.py
@@ -24,6 +24,57 @@ class singlyLinkedList:
             newNode.nextNode=self.headNode
             self.headNode=newNode
 
+    def insertAtIndex(self,index,value):
+        newNode=Node(value)
+        if index==0:
+            newNode.nextNode=self.headNode
+            self.headNode=newNode
+            return
+        else:
+            currentNode=self.headNode
+            currentPosition=0
+            while currentNode is not None and currentPosition<index-1:
+                currentNode=currentNode.nextNode
+                currentPosition+=1
+            if currentNode is None:
+                print("Index out of bounds")
+            else:
+                newNode.nextNode=currentNode.nextNode
+                currentNode.nextNode=newNode
+
+    def deleteFromBeginning(self):
+        if self.headNode is None:
+            print("This list is empty, nothing to delete.")
+        else:
+            self.headNode=self.headNode.nextNode
+
+    def deleteFromEnd(self):
+        if self.headNode is None:
+            print("This list is empty, nothing to delete")
+        elif self.headNode.nextNode is None:
+            self.headNode=None
+        else:
+            currentNode=self.headNode
+            while currentNode.nextNode.nextNode is not None:
+                currentNode=currentNode.nextNode
+            currentNode.nextNode=None
+
+    def deleteFromIndex(self,index):
+        if self.headNode is None:
+            print("The list is empty, nothing to delete.")
+        elif index==0:
+            self.headNode=self.headNode.nextNode
+        else:
+            currentNode=self.headNode
+            currentPosition=0
+            while currentNode is not None and currentPosition<index-1:
+                currentNode=currentNode.nextNode
+                currentPosition+=1
+            if currentNode is None or currentNode.nextNode is None:
+                print("Index out of bounds")
+            else:
+                currentNode.nextNode=currentNode.nextNode.nextNode
+
 
     def showListItems(self):
         currentNode=self.headNode
@@ -43,4 +94,9 @@ listOfNumbers=singlyLinkedList()
 listOfNumbers.insertAtEnd(1)
 listOfNumbers.insertAtEnd(2)
 listOfNumbers.insertAtEnd(3)
+listOfNumbers.showListItems()
+
+listOfNames.deleteFromEnd()
+listOfNames.showListItems()
+listOfNumbers.deleteFromBeginning()
 listOfNumbers.showListItems()
