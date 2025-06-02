3ed6ca5f215ad32688c8d8f3af4407b725466c99
Karanei-Kimutai <168116985+Karanei-Kimutai@users.noreply.github.com>
2025-06-02 11:22:53 +0300

    Added a new implementation

--- Queues/palindromeCheckUsingQueues.py
+++ Queues/palindromeCheckUsingQueues.py
@@ -10,6 +10,21 @@ def palindromeCheckUsingQueues(string):
         print("This string is not a palindrome")
         return False
 
+def palindromeCheckUsingQueues2(string):
+    queue=[]
+    for character in string:
+        queue.append(character)
+    reversedString=""
+    while len(queue)!=0:
+        reversedString+=queue.pop()
+    if string == reversedString:
+        print("This string is a palindrome")
+        return True
+    else:
+        print("This string is not a palindrome")
+        return False
+
 
 userInput=input("Enter the string you want to test: ").strip().lower().replace(" ","")
 palindromeCheckUsingQueues(userInput)
+palindromeCheckUsingQueues2(userInput)
